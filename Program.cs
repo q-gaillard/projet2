b5dd61dbbeae13af36bbdd7c5158762c70adcb0d
q-gaillard <quentin.gaillard@edouardgand.fr>
2026-03-11 13:24:37 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Data.Common;
+using System.Diagnostics.Contracts;
+using System.Diagnostics.Tracing;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace projet_C_;
+
+class Compte
+{
+    
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
